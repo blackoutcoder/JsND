4ee7619960193bc726a13b31dbc5acdd7f95555b
blackoutcoder <romka19841201@gmail.com>
2022-02-12 23:12:21 +0200

    Else if, If , Else

---
+++ pratimas_3/script.js
@@ -0,0 +1,12 @@
+//Sukurkite kintamajį pavadinimu age ir jam priskirkite reikšmę 19. Parašykite programą kuri atvaizduotų (keiskite age reikšmę, kad patestuoti ar programa veikia) 
+//Jeigu amžius yra mažesnis nei 18 – atvaizduoja nepilnametis. 
+//Jeigu amžius yra tarp 18 ir 20 – atvaizduoja pilnametis, bet vartoti alkoholio negalima. 
+//Jeigu amžius yra virš 20 – pilnametis, vartoti alkoholį galima. 
+let age = 21;
+if (age < 18)
+    document.write("Nepilnametis");
+else if (age >= 18, age <= 20)
+    document.write("Pilnametis, bet vartoti alkoholio negalima");
+else 
+    document.write("Pilnametis, gali vartoti alkoholį");     
+//Darbas užeme apie 10 min
