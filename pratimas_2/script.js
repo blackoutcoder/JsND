830b7a86af35eac49abbc5ebca09014a6a95c1a7
blackoutcoder <romka19841201@gmail.com>
2022-02-12 22:46:16 +0200

    IF operator

---
+++ pratimas_2/script.js
@@ -0,0 +1,9 @@
+//Sukurkite kintamajį pavadinimu age(metai) ir jam priskirtike reikšmę 17. (age = 17). Parašykite programą kuri atvaizduotų (keiskite age reikšmę, kad patestuoti ar programa veikia) Info: https://www.w3schools.com/js/js_if_else.asp  
+//Jeigu amžius yra mažesnis nei 18 – atvaizduoja vairuoti negali ir kiek trūksta metų iki 18. 
+//Jeigu amžius yra didesnis nei 18 – atvaizduoja vairuoti gali. 
+let age = 17;
+if (age >= 18) 
+    document.write("Vairuoti gali!!!");
+else
+    document.write("Vairuoti negali");    
+//Darbas užeme 2 minutes.
