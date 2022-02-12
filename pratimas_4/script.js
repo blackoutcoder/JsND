5f2fa647d66e790a04b76be0d94fca7a651961d6
blackoutcoder <romka19841201@gmail.com>
2022-02-12 23:27:38 +0200

    Ternary operator (?).

---
+++ pratimas_4/script.js
@@ -0,0 +1,7 @@
+//Parašykite programą naudodami 2 užduoties logiką, bet susikurkite variable pavadinimu isDrivingAllowed ir panaudokite ternary išraišką.
+//If statemente naudoti isDrivingAllowed kintamajį.
+
+let age = 18;
+let isDrivingAllowed = (age < 18)? "Vairuoti negali" : "Vairuoti gali";
+document.write(isDrivingAllowed); 
+//Darbas užeme 5min (darbas + info);
