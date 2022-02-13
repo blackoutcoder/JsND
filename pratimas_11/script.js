57864d7eb2629f05e4564d29d461c0623524640c
blackoutcoder <romka19841201@gmail.com>
2022-02-13 20:55:17 +0200

    lenght(part2).

---
+++ pratimas_11/script.js
@@ -0,0 +1,19 @@
+//Parašykite programą kuri atvaizduos kiekvieną jūsų vardo raidę ir kelinta raidė tai yra. 
+//Pvz. Pirmas ciklas turi rodyti R raidė yra 1, 
+//Antras ciklas turi rodyti a raidė yra 2 ir t.t. 
+let browserType = 'Roman';
+browserType.length;
+browserType[1];
+
+
+//let simpleNumbers = "";
+//for (let i = 1; i < 6; i++) {
+//    simpleNumbers = simpleNumbers + i;   
+//  }
+document.write("1 - " + browserType[0]);
+document.write("<br/>" + "2 - " + browserType[1]);
+document.write("<br/>" + "3 - " + browserType[2]);
+document.write("<br/>" + "4 - " + browserType[3]);
+document.write("<br/>" + "5 - " + browserType[4]);
+ 
+//Darbas užeme apie 30 min, nieko protingesnio nesugalvojau :( 
