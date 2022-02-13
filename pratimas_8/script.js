4743b647fcb5d72c3d603a9231a97dbf635f5416
blackoutcoder <romka19841201@gmail.com>
2022-02-13 05:52:05 +0200

    For operator(part_2).

---
+++ pratimas_8/script.js
@@ -0,0 +1,9 @@
+//Parašykite programą kuri atvaizduos skaičius nuo -10 iki -20.
+let simpleNumbers = "";
+
+for (let i = -10; i > -21; i--) {
+  simpleNumbers = simpleNumbers + i;
+}
+document.write(simpleNumbers);
+
+//Darbas užeme apie ~ 10 - 15 min.
