15e58205526cb5cf19d10e191a475dfa8d6b595e
blackoutcoder <romka19841201@gmail.com>
2022-02-13 07:01:24 +0200

    For operator(part_3).

---
+++ pratimas_9/script.js
@@ -0,0 +1,9 @@
+//Parašykite programą kuri atvaizduos kas antrą skaičių nuo 0 iki 20.
+
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 == 0) {
+      document.write(i);
+    }
+  }
+
+//Darbas užeme apie 1į min :)
