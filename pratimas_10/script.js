2236ebb58bcf63cf20b73085627960386f7a1979
blackoutcoder <romka19841201@gmail.com>
2022-02-13 20:01:04 +0200

    variable.lenght

---
+++ pratimas_10/script.js
@@ -0,0 +1,8 @@
+//Sukurkite string variable name ir priskirkite reikšmę savo vardą. 
+//Pvz name = 'Rokas'. Parašykite programą kuri atvaizduos kokio ilgio yra jūsų vardas. 
+//T.y. Kiek raidžių yra jūsų varde. (pagal mano duotą pvz. Tūrėtų būti atsakymas 5) 
+
+let firstName = "Roman";
+document.write(firstName.length); 
+
+//Darbas užeme apie 10 min. Gal aš ne taip supreatau užduoti, labai paprasta :)
