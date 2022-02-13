6d9cc84375082f145a2328357ce3492d6db9b197
blackoutcoder <romka19841201@gmail.com>
2022-02-13 05:21:29 +0200

    For operator.

---
+++ pratimas_7/script.js
@@ -0,0 +1,9 @@
+//Parašykite programą kuri atvaizduos skaičius nuo 0 iki 9.
+let simpleNumbers = "";
+
+for (let i = 0; i < 10; i++) {
+  simpleNumbers = simpleNumbers + i;
+}
+document.write(simpleNumbers);
+
+//Darbas užeme apie 10 min.
